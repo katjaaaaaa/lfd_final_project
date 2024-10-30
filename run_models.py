5fa8509e33854369b52f3bf951a704c3d3935123
katjaaaaaa <katkamshova@gmail.com>
2024-10-30 13:44:40 +0100

    updated the code with lstm and llm calls

--- run_models.py
+++ run_models.py
@@ -1,14 +1,27 @@
+# bad ass words https://github.com/zacanger/profane-words/blob/master/words.json
+
 import logging
 import argparse
 import pandas as pd
+import numpy as np
+import nltk
+from nltk.stem import WordNetLemmatizer
 from itertools import combinations
-from sklearn.feature_extraction.text import CountVectorizer
+from sklearn.feature_extraction.text import CountVectorizer, TfidfVectorizer
 from sklearn.naive_bayes import MultinomialNB
+from sklearn.pipeline import FeatureUnion
 from sklearn.metrics import classification_report
+from sklearn.utils import class_weight
+import json
+import random
+import lstm
+import transformer
 
+
+random.seed(5)
 
 # Setup logging configuration
-logging.basicConfig(filename='results.log', level=logging.INFO,
+logging.basicConfig(filename='logs.log', level=logging.INFO,
                     format='%(asctime)s - %(message)s')
 
 
@@ -22,16 +35,65 @@ def log_and_print(message, printed=True):
 
 def create_arg_parser():
     parser = argparse.ArgumentParser()
+
+    # GENERAL STUFF
     parser.add_argument("-d","--data", default=False, action="store_true",
                         help="Provides information about the datasets")
-    parser.add_argument("-t", "--test", default=False, action="store_true", # change so it catches the file name as well
-                        help="Runs the model on the test set")
-    parser.add_argument("-m", "--model", default="nb", choices=["nb", "x", "lstm", "llm"],
+    parser.add_argument("-log", "--log", default=False, action="store_true",
+                        help="Logs the output and arguments")
+    parser.add_argument("-md", "--make_dev", default=False, action="store_true", # change so it catches the file name as well
+                        help="Create a new dev file")
+    parser.add_argument("-t", "--test", default="False", action="store_true",
+                        help="Run the model on the test set")
+    parser.add_argument("-mt", "--make_test", default=False, action="store_true", # change so it catches the file name as well
+                        help="Create a new test file")
+    parser.add_argument("-m", "--model", default="nb", choices=["nb", "lstm", "llm"],
                         help="Defines the model to make the predictions")
+    
+    # BASELINE STUFF
     parser.add_argument("-vec", "--vectorizer", choices=["bow", "tfidf", "both"],
                         default="bow", help="Select vectorizer: bow (bag of words), tfidf or both")
-    args = parser.parse_args()
+    parser.add_argument("-ng", "--ngram_range", nargs=2, type=int, default=(1, 1),
+                        help="Set the ngram range, give two integers separated by space")
+    parser.add_argument("-lemma", "--lemmas", action="store_true",
+                        help="Lemmatizes the tokenized data.")
+    parser.add_argument("-w", "--weights", default=False, action="store_true",
+                        help="Adds weights to the baseline classifier")
 
+    # LSTM STUFF
+    parser.add_argument("-bi", "--bidirectional", action="store_true",
+                        help="If added, use a bidirectional LSTM")
+    parser.add_argument("-lr", "--learning_rate", default=0.01, type=float,
+                    help="Set the learning rate")
+    parser.add_argument("-ww", "--class_weights", nargs=2, type=int, default=[1, 1],
+                        help="Sets the class weights to balance the data")
+    parser.add_argument("-l", "--loss_function", default="binary_crossentropy", type=str,
+                        help="Set the loss function")
+    parser.add_argument("-a", "--activation", default="sigmoid", type=str,
+                        help="Set the activation")
+    parser.add_argument("-ah", "--activation_hidden", default="sigmoid", type=str,
+                        help="Set the activation for the hidden layer")
+    parser.add_argument("-bs", "--batch_size", default=16, type=int,
+                        help="Set the batch size")
+    parser.add_argument("-ep", "--epochs", default=50, type=int,
+                        help="Set the number of epochs")
+    parser.add_argument("-mo", "--momentum", default=0.9, type=float,
+                        help="Controls the influence of previous epoch on the next weight update")
+    parser.add_argument("-es", "--early_stop", default=3, type=int,
+                        help="Set the patience of early stop")
+    parser.add_argument("-o", "--optimizer", default="adam", choices=["sgd", "adam"],
+                        help="Select optimizer (SGD, ADAM)")
+    parser.add_argument("-dr", "--dropout", default=None, type=float,
+                        help="Set a dropout layer")
+    parser.add_argument("-ex", "--extra_layer", default=0, type=int, choices=[1, 2],
+                        help="Set an amount of extra layers, max 2 extra layers, keeps the same settings as the base layer.")
+
+    # TRANSFORMERS STUFF
+    parser.add_argument("-tr", "--transformer", default="roberta", choices=["distilbert", "roberta", "electra", "deberta"])
+
+
+
+    args = parser.parse_args()
     return args
 
 
@@ -46,19 +108,38 @@ def select_classifier(args):
 
     return model
 
-def data_preprocess(data):
+def data_preprocess(data, off=False):
+
+    data_new = data.copy(deep=True)
 
     # replace necessary words
-    data["text"] = data["text"].str.replace('@USER', 'usersign')
-    data["text"] = data["text"].str.replace(r"#\w+", "hashtagsign", regex=True)
+    data_new["text"] = data_new["text"].str.replace('@USER ', '')
+    data_new["text"] = data_new["text"].str.replace('URL', '')
 
-    # remove all characters except for ascii supported letters and numbers
-    filter_char = lambda c: ord(c) < 256
-    #data["text"] = data["text"].apply(lambda s: ''.join(filter(filter_char, s)))
-    data["text"] = data["text"].apply(lambda x: x.lower())
-    data["text"] = data["text"].str.replace(r'[^\w\s]', '', regex=True)
+    data_new["text"] = data_new["text"].apply(lambda x: x.lower())
 
-    return data
+    if off:
+        with open('./data/words.json') as f:
+            off_words = json.load(f)
+        print("Start preprocessing data")
+        for word in off_words:
+            data_new["text"] = data_new["text"].str.replace(rf"\b{word}\b", "offword", regex=True)
+        #print(data_new[data_new["text"].str.contains("offword", na=False)].head)
+        print("Data preprocessed")
+
+    data_new["text"] = data_new["text"].str.replace(r'[^\w\s]', '', regex=True)
+
+
+    #nltk.download('punkt_tab')
+    # if args.ntopwords:
+    #     tt = nltk.tokenize.TweetTokenizer()
+    #     tokens = sum([tt.tokenize(line) for line in data_new["text"]], [])
+    #     tokens_dict = nltk.FreqDist(w for w in tokens)
+    #     for k in tokens_dict.keys():
+    #         if k != "offword":
+    #             data_new["text"] = data_new["text"].str.replace(rf"\b{k}\b", "", regex=True)
+
+    return data_new
 
 
 def data_count(train, dev, test):
@@ -78,20 +159,36 @@ def select_vectorizer(args):
     Initialize the vectorizer based on the given arguments.
     """
     # Initialize vectorizers with selected arguments.
-    #tf_idf = TfidfVectorizer(preprocessor=identity, tokenizer=identity, ngram_range=tuple(arguments.ngram_range))
-    #bow = CountVectorizer(preprocessor=identity, tokenizer=identity, ngram_range=tuple(arguments.ngram_range))
-    bow = CountVectorizer()
-    #union = FeatureUnion([("count", bow), ("tf", tf_idf)])
+    tf_idf = TfidfVectorizer(preprocessor=identity, tokenizer=identity, ngram_range=tuple(args.ngram_range), token_pattern=None)
+    if args.lemmas:
+        bow = CountVectorizer(analyzer="word",preprocessor=identity, tokenizer=identity, ngram_range=tuple(args.ngram_range), token_pattern=None)
+    else:
+        bow = CountVectorizer(analyzer="char", ngram_range=tuple(args.ngram_range))
+
+    union = FeatureUnion([("count", bow), ("tf", tf_idf)])
 
     if args.vectorizer == "tfidf":
-        pass
-        # return tf_idf
+        return tf_idf
     elif args.vectorizer == "bow":
         # Bag of Words vectorizer
         return bow
     elif args.vectorizer == "both":
-        pass
-        # return union
+        return union
+
+
+def identity(inp):
+    """
+    Returns the input, or the lemmatized input if the user has chosen
+    lemmatization.
+    """
+    if args.lemmas:
+        lemmatizer = WordNetLemmatizer()
+        lemma_list = [lemmatizer.lemmatize(word) for word in inp]
+
+        return lemma_list
+
+    return inp
+
 
 
 def run_model(args, train, test):
@@ -100,46 +197,52 @@ def run_model(args, train, test):
     model = select_classifier(args)
 
     X_train = vectorizer.fit_transform(train["text"])
-    X_dev = vectorizer.transform(test["text"])
+    X_test = vectorizer.transform(test["text"])
 
     y_train = train["off"]
-    y_dev = test["off"]
+    y_test = test["off"]
 
-    model.fit(X_train, y_train)
+    if args.weights:
+        c_w = class_weight.compute_class_weight(class_weight={"NOT" : 1, "OFF": 2}, classes=np.unique(y_train), y=y_train)
+        c_w = {"NOT" : c_w[0], "OFF" : c_w[1]}
+        model.fit(X_train, y_train, sample_weight=[c_w[i] for i in y_train.to_numpy()])
+    else:
+        model.fit(X_train, y_train)
+    y_pred = model.predict(X_test)
+    if args.log:
+        log_and_print(classification_report(y_test, y_pred))
+    else:
+        print(classification_report(y_test, y_pred))
 
-    y_pred = model.predict(X_dev)
-
-    print(classification_report(y_dev, y_pred))
 
 
-def prepare_for_triple(args, train, test):
+def prepare_data(args, train, test, mode="dev"):
 
-    train_full, test_full = train, test
-    train_hash, test_hash = train, test
-    train_user, test_user = train, test
+    full_train = data_preprocess(train)
+    full_test = data_preprocess(test)
 
-    # data with hashtags only
-    train_hash = train[train["text"].str.contains(r"\bhashtagsign\b", case=False, na=False)]
-    test_hash = test[test["text"].str.contains(r"\bhashtagsign\b", case=False, na=False)]
-    train_hash.loc[:, "text"] = train_hash["text"].str.replace('usersign', '')
-    test_hash.loc[:, "text"] = test_hash["text"].str.replace('usersign', '')
+    # Preprocess all the off_data and store in the files
+    if args.make_dev or args.make_test:
+        off_train = data_preprocess(train, True)
+        off_test = data_preprocess(test, True)
+        off_train.to_csv('./data/off_train.tsv', sep='\t', index=False)
+        if mode == "test":
+            off_test.to_csv('./data/off_test.tsv', sep='\t', index=False)
+        else:
+            off_test.to_csv('./data/off_dev.tsv', sep='\t', index=False)
+    else:
+        # Load already created datasets
+        off_train = pd.read_csv("./data/off_train.tsv", sep='\t')
 
-    # data with user mentions only
-    train_user = train[train["text"].str.contains(r"\busersign\b", case=False, na=True)]
-    test_user = test[test["text"].str.contains(r"\busersign\b", case=False, na=True)]
-    train_user.loc[:, "text"] = train_user["text"].str.replace('hashtagsign', '')
-    test_user.loc[:, "text"] = test_user["text"].str.replace('hashtagsign', '')
+        if mode == "test":
+            off_test = pd.read_csv("./data/off_test.tsv", sep='\t')
+        else:
+            off_test = pd.read_csv("./data/off_dev.tsv", sep='\t')
 
-    data_list = [train_full, test_full, train_hash, test_hash, train_user, test_user]
+    return full_train, full_test, off_train, off_test
 
-    print("FULL DATA")
-    run_model(args, train_full, test_full)
-    print("HASH ONLY DATA")
-    run_model(args, train_hash, test_hash)
-    print("USER ONLY DATA")
-    run_model(args, train_user, test_user)
 
-def main():
+if __name__ == "__main__":
     args = create_arg_parser()
 
     colnames = ["text", "off"]
@@ -151,18 +254,41 @@ def main():
         data_count(df_train, df_dev, df_test)
         exit()
 
-    df_train = data_preprocess(df_train)
-    df_dev = data_preprocess(df_dev)
-    df_test = data_preprocess(df_test)
 
-    if args.test:
-        pass
-        #run_model(args, df_train, df_test)
-    else:
-        prepare_for_triple(args, df_train, df_dev)
-        #run_model(args, df_train, df_dev)
+    # run the baseline
+    if args.model == "nb":
+        if args.test:
+            full_train, full_test, off_train, off_test = prepare_data(args, df_train, df_test, "test")
+        else:
+            full_train, full_test, off_train, off_test = prepare_data(args, df_train, df_dev)
+
+        print("FULL DATA")
+        run_model(args, full_train, full_test)
+        print("NO OFF DATA")
+        run_model(args, off_train, off_test)
+
+    # run lstm
+    elif args.model == "lstm":
+        full_train, full_test, off_train, off_test = prepare_data(args, df_train, df_test, "test")
+        _, full_dev, _, off_dev = prepare_data(args, df_train, df_dev)
+
+        print("FULL DATA")
+        lstm.run(full_train, full_dev, full_test, args)
+    
+        print("NO OFF DATA")
+        lstm.run(off_train, off_dev, off_test, args)
+
+    elif args.model == "llm":
+        full_train, full_test, off_train, off_test = prepare_data(args, df_train, df_test, "test")
+        _, full_dev, _, off_dev = prepare_data(args, df_train, df_dev)
+
+        print("FULL DATA")
+        transformer.run(full_train, full_dev, full_test, args)
+    
+        print("NO OFF DATA")
+        transformer.run(off_train, off_dev, off_test, args)
 
 
-
-if __name__ == "__main__":
-    main()
+    if args.log:
+        all_args = " \\\n".join([f" --{key}={value}" for key, value in vars(args).items() if value])
+        log_and_print(f"Used settings:\n{all_args}", False)
